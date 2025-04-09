8b0041da1909a45893ed3842af3ace488d756db1
Arnaud Spiwack <arnaud.spiwack@tweag.io>
2025-04-09 21:49:29 +0900

    Generic mechanism to derive leads

--- src/Examples.hs
+++ src/Examples.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE QualifiedDo #-}
 
@@ -5,35 +6,22 @@ module Examples where
 
 import Combinators
 import Control.Monad.Indexed ((<*), (<*>), (<|>))
-import Control.Monad.Indexed qualified as Indexed
+import GHC.Generics
+import Generic (lead)
 import Prelude hiding (Applicative (..), Monad (..))
 
 data T = C Int Bool | D Char Bool Int
-  deriving (Show)
+  deriving (Show, Generic)
 
 data U = K T Int | L
-  deriving (Show)
+  deriving (Show, Generic)
 
 uupT :: PUP (T -> r) r T
 uupT =
-  cLead <* string "C" <* space <*> int <* space <*> bool
-    <|> dLead <* string "D" <* space <*> anyChar <* space <*> bool <* space <*> int
-  where
-    cLead = Indexed.do
-      Indexed.stack (\cases _ k (C i b) -> k i b; fl _ t -> fl t) (\k i b -> k (C i b))
-      Indexed.pure C
-    dLead = Indexed.do
-      Indexed.stack (\cases _ k (D c b i) -> k c b i; fl _ t -> fl t) (\k c b i -> k (D c b i))
-      Indexed.pure D
+  lead @"C" @T <* string "C" <* space <*> int <* space <*> bool
+    <|> lead @"D" @T <* string "D" <* space <*> anyChar <* space <*> bool <* space <*> int
 
 uupU :: PUP (U -> r) r U
 uupU =
-  kLead <* string "K" <* space <*> uupT <* space <*> int
-    <|> lLead <* string "L"
-  where
-    kLead = Indexed.do
-      Indexed.stack (\cases _ k (K t i) -> k t i; fl _ t -> fl t) (\k t i -> k (K t i))
-      Indexed.pure K
-    lLead = Indexed.do
-      Indexed.stack (\cases _ k L -> k; fl _ t -> fl t) (\k -> k L)
-      Indexed.pure L
+  lead @"K" @U <* string "K" <* space <*> uupT <* space <*> int
+    <|> lead @"L" @U <* string "L"
