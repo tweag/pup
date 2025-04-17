33889873e1fac62ebdf43e06a763a57ce86067ad
Arnaud Spiwack <arnaud.spiwack@tweag.io>
2025-04-17 17:43:36 +0900

    Implement Print with a comonad

--- src/Base/Print.hs
+++ src/Base/Print.hs
@@ -1,47 +1,24 @@
 {-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE QualifiedDo #-}
 
 module Base.Print where
 
-import Control.Applicative
-import Control.Monad
+import Control.Comonad.Traced qualified as Comonad
 import Control.Monad.Indexed qualified as Indexed
+import Control.Monad.Indexed.Cont2 (Cont2W)
+import Control.Monad.Indexed.Cont2 qualified as Cont2
 import Prelude hiding (Applicative (..), Monad (..))
-import Prelude qualified
 
-newtype Print r r' a = Print {runPrint :: r -> (a -> String -> r' -> r') -> r}
-  deriving stock (Functor)
-
-deriving via (Indexed.FromIndexed Print r r) instance Prelude.Applicative (Print r r)
-
-deriving via (Indexed.FromIndexed Print r r) instance Prelude.Monad (Print r r)
-
-deriving via (Indexed.FromIndexed Print r r) instance Alternative (Print r r)
-
-deriving via (Indexed.FromIndexed Print r r) instance MonadPlus (Print r r)
-
-instance Indexed.Applicative Print where
-  pure x = Print $ \fl k -> k x "" fl
-
-instance Indexed.Monad Print where
-  (Print a) >>= kp = Print $ \fl k -> a fl $ \x sx flx ->
-    runPrint (kp x) flx $ \y sy fly -> k y (sx ++ sy) fly
-
-instance Indexed.Stacked Print where
-  empty = Print $ \fl _k -> fl
-  (Print lft) <|> (Print rgt) = Print $ \fl k ->
-    lft (rgt fl k) k
-
-  -- stack f unr = Print $ \fl k -> f fl (k () mempty (unr fl))
-  shift' f = shift (\k -> f (k () ""))
+type Print = Cont2W (Comonad.Traced String)
 
 print :: forall r a. (Indexed.Unroll r (Maybe String)) => Print r (Maybe String) a -> r
-print prnt = runPrint prnt (Indexed.unroll @r @(Maybe String) Nothing) (\_ s _ -> Just s)
+print prnt = Cont2.runCont2W prnt (Comonad.traced (\s _ _ -> Just s)) (Indexed.unroll @r @(Maybe String) Nothing)
 
 anyChar :: Print (Char -> r) r Char
-anyChar = Print $ \fl k c -> k c [c] (fl c)
+anyChar =
+  Cont2.shift $ \wk fl -> Indexed.pure $ \c -> Comonad.runTraced wk [c] c (fl c)
 
 once :: (r -> r') -> Print r r' a -> Print r r' a
-once unr (Print a) = Print $ \fl k -> a fl $ \x sx _flx -> k x sx (unr fl)
-
-shift :: ((a -> String -> r' -> r') -> r -> Print r r'' r'') -> Print r r' a
-shift f = Print $ \fl k -> runPrint (f k fl) fl (\k' _ _ -> k')
+once unr prnt = Cont2.shift $ \wk fl -> Indexed.do
+  a <- prnt
+  Indexed.pure $ Comonad.extract wk a (unr fl)
--- src/Combinators.hs
+++ src/Combinators.hs
@@ -6,7 +6,7 @@ module Combinators where
 
 import Base.Parse (Parse (..))
 import Base.Parse qualified as Parse
-import Base.Print (Print (..))
+import Base.Print (Print)
 import Base.Print qualified as Print
 import Control.Monad
 import Control.Monad.Indexed ((<*), (<|>))
