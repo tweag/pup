3a005b3fe631867f230bdbe81b23f7ecc9cdaee8
Arnaud Spiwack <arnaud.spiwack@tweag.io>
2025-06-06 14:57:13 +0900

    Random, ineffective thoughts about multimonads

--- src/SelfContained/Tr1.hs
+++ src/SelfContained/Tr1.hs
@@ -8,86 +8,97 @@ import Control.Comonad.Store
 import Control.Comonad.Traced
 import Prelude hiding (id, (.))
 
-data (p :*: q) r r' = (:*:) {cfst :: p r r', csnd :: q r r'}
+-- data (p :*: q) r r' = (:*:) {cfst :: p r r', csnd :: q r r'}
 
-instance (Category p, Category q) => Category (p :*: q) where
-  id = id :*: id
-  (p1 :*: q1) . (p2 :*: q2) = (p1 . p2) :*: (q1 . q2)
+-- instance (Category p, Category q) => Category (p :*: q) where
+--   id = id :*: id
+--   (p1 :*: q1) . (p2 :*: q2) = (p1 . p2) :*: (q1 . q2)
 
-type K9 = Cokleisli (Traced String) :*: Cokleisli (Store String)
+-- type K9 = Cokleisli (Traced String) :*: Cokleisli (Store String)
 
--- Yes, I can't be bothered to import Profunctor.
-class (Category p) => CProf p where
-  arr :: (a -> b) -> p a b
+-- -- Yes, I can't be bothered to import Profunctor.
+-- class (Category p) => CProf p where
+--   arr :: (a -> b) -> p a b
 
-instance (CProf p, CProf q) => CProf (p :*: q) where
-  arr f = arr f :*: arr f
+-- instance (CProf p, CProf q) => CProf (p :*: q) where
+--   arr f = arr f :*: arr f
 
-instance (Comonad w) => CProf (Cokleisli w) where
-  arr f = Cokleisli $ \wa -> f (extract wa)
+-- instance (Comonad w) => CProf (Cokleisli w) where
+--   arr f = Cokleisli $ \wa -> f (extract wa)
 
-class Stacked p where
-  shift_ :: (r -> p (k -> k) r') -> p r r'
+-- class Stacked p where
+--   shift_ :: (r -> p (k -> k) r') -> p r r'
 
-push :: (Stacked p, CProf p) => a -> p (a -> r) r
-push a = shift_ $ \k -> arr (\_ -> k a)
+-- push :: (Stacked p, CProf p) => a -> p (a -> r) r
+-- push a = shift_ $ \k -> arr (\_ -> k a)
 
-pop_ :: (Stacked p, CProf p) => p r (a -> r)
-pop_ = shift_ $ \k -> arr (\_ _ -> k)
+-- pop_ :: (Stacked p, CProf p) => p r (a -> r)
+-- pop_ = shift_ $ \k -> arr (\_ _ -> k)
 
-instance (Comonad w) => Stacked (Cokleisli w) where
-  shift_ f = Cokleisli $ \wk -> runCokleisli (f (extract wk)) (const id <$> wk)
+-- instance (Comonad w) => Stacked (Cokleisli w) where
+--   shift_ f = Cokleisli $ \wk -> runCokleisli (f (extract wk)) (const id <$> wk)
 
-instance (Stacked p, Stacked q) => Stacked (p :*: q) where
-  shift_ f = shift_ (cfst . f) :*: shift_ (csnd . f)
+-- instance (Stacked p, Stacked q) => Stacked (p :*: q) where
+--   shift_ f = shift_ (cfst . f) :*: shift_ (csnd . f)
 
-class Switching p where
-  switch :: (a -> p r r') -> p r (a -> r')
+-- class Switching p where
+--   switch :: (a -> p r r') -> p r (a -> r')
 
-instance (Switching p, Switching q) => Switching (p :*: q) where
-  switch f = switch (cfst . f) :*: switch (csnd . f)
+-- instance (Switching p, Switching q) => Switching (p :*: q) where
+--   switch f = switch (cfst . f) :*: switch (csnd . f)
 
-instance (Comonad w) => Switching (Cokleisli w) where
-  switch f = Cokleisli $ \wk a -> runCokleisli (f a) wk
+-- instance (Comonad w) => Switching (Cokleisli w) where
+--   switch f = Cokleisli $ \wk a -> runCokleisli (f a) wk
 
-class Descr p where
-  satisfy :: (Char -> Bool) -> p (Char -> r) (Char -> r)
+-- class Descr p where
+--   satisfy :: (Char -> Bool) -> p (Char -> r) (Char -> r)
 
-instance (Descr p, Descr q) => Descr (p :*: q) where
-  satisfy f = satisfy f :*: satisfy f
+-- instance (Descr p, Descr q) => Descr (p :*: q) where
+--   satisfy f = satisfy f :*: satisfy f
 
-instance Descr (Cokleisli (Traced String)) where
-  satisfy _f = Cokleisli $ \(runTraced -> k) c -> k [c] c
+-- instance Descr (Cokleisli (Traced String)) where
+--   satisfy _f = Cokleisli $ \(runTraced -> k) c -> k [c] c
 
-instance Descr (Cokleisli (Store String)) where
-  satisfy f = Cokleisli $ \(runStore -> (k, s)) _c -> case s of
-    [] -> error "end of input"
-    (c : cs)
-      | f c -> k cs c
-      | otherwise -> error $ "character '" ++ [c] ++ "' unexpected"
+-- instance Descr (Cokleisli (Store String)) where
+--   satisfy f = Cokleisli $ \(runStore -> (k, s)) _c -> case s of
+--     [] -> error "end of input"
+--     (c : cs)
+--       | f c -> k cs c
+--       | otherwise -> error $ "character '" ++ [c] ++ "' unexpected"
 
-char :: (Descr p, Stacked p, CProf p) => p (Char -> r) (Char -> r)
-char = satisfy (const True)
+-- char :: (Descr p, Stacked p, CProf p) => p (Char -> r) (Char -> r)
+-- char = satisfy (const True)
 
-digit :: (Descr p, Stacked p, CProf p) => p (Int -> r) (Int -> r)
-digit = arr (\k -> k . head . show) . char . arr (\k -> k . read . (: []))
+-- digit :: (Descr p, Stacked p, CProf p) => p (Int -> r) (Int -> r)
+-- digit = arr (\k -> k . head . show) . char . arr (\k -> k . read . (: []))
 
-lit :: (Descr p, Stacked p, CProf p) => String -> p r r
-lit [] = arr id
-lit (c : cs) =
-  push c . satisfy (== c) . pop_ . lit cs
+-- lit :: (Descr p, Stacked p, CProf p) => String -> p r r
+-- lit [] = arr id
+-- lit (c : cs) =
+--   push c . satisfy (== c) . pop_ . lit cs
 
-pretty :: K9 (a -> String) (b -> String) -> b -> String
-pretty (Cokleisli pr :*: _) b = pr (traced (\s _ -> s)) b
+-- pretty :: K9 (a -> String) (b -> String) -> b -> String
+-- pretty (Cokleisli pr :*: _) b = pr (traced (\s _ -> s)) b
 
-parse :: K9 (a -> a) (b -> a) -> String -> a
-parse (_ :*: Cokleisli pa) s = pa (store (\_ a -> a) s) (error "I so swear")
+-- parse :: K9 (a -> a) (b -> a) -> String -> a
+-- parse (_ :*: Cokleisli pa) s = pa (store (\_ a -> a) s) (error "I so swear")
 
-spec = digit . lit "−th character after " . char . lit " is " . char
+-- spec = digit . lit "−th character after " . char . lit " is " . char
 
--- newtype Tr r r' = Tr { unTr :: (String -> r) -> (String -> r') }
+newtype Tr r r' = Tr {unTr :: r -> r'}
 
--- newtype Flp p r r' = Flp { unFlp :: forall k. p (r -> k) (r' -> k)}
+newtype Flp p r r' = Flp {unFlp :: forall k. p (r -> k) (r' -> k)}
+
+switch :: (a -> Flp Tr r r') -> Flp Tr (a -> r) r'
+switch f = Flp $ Tr $ \k k' -> k (\a -> unTr (unFlp (f a)) id k')
+
+switchw :: (Comonad w) => (a -> Flp (Cokleisli w) r r') -> Flp (Cokleisli w) (a -> r) r'
+switchw f = Flp $ Cokleisli $ \k k' -> extract k (\a -> runCokleisli (unFlp (f a)) (const id <$> k) k')
+
+newtype Pr r r' o o' = Pr {unPr :: (o' -> r) -> (o -> r')}
+
+switchd :: (a -> Pr r r' o o') -> Pr r r' o (a -> o')
+switchd f = Pr $ \k ok -> _
 
 -- class Switching p where
 --   switch :: (a -> p r r') -> p r (a -> r')
